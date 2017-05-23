adc6d44b2901d10d0116a66808652251b01e9114
Robert Scheller <rmscheller@gmail.com>
2017-05-23 16:15:08 -0700

    Unzipped input data.

---
+++ ClimateChangeFuelTreatments_Low-Ignitions/RunScenarios.bat
@@ -0,0 +1,21 @@
+rem set name=scenario-A2-lower-ign_PDSItester
+rem for %%i in (1) do call helprun %name% %%i
+
+set name=scenario-A2-Continuous-15RP_insects
+for %%i in (15) do call helprun %name% %%i
+
+
+rem set name=scenario-A2-lower-ign
+rem for %%i in (10 11 12 13 14) do call helprun %name% %%i
+
+rem set name=scenario-A2-Continuous-15RP
+rem for %%i in (15 16 17 18 19 20) do call helprun %name% %%i
+
+
+rem set name=scenario-base-Continuous-15RP
+rem for %%i in (1 2 3) do call helprun %name% %%i
+
+rem set name=scenario-base-Continuous-15RP_insects
+rem for %%i in (10) do call helprun %name% %%i
+
+pause
